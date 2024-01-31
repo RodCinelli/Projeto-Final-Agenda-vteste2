1baa14df2d312178f38833e806d96c5645e0b956
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-01-31 02:01:49 -0300

    Teste

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -16,18 +16,36 @@ export class AuthService {
 
   constructor(
     private afAuth: AngularFireAuth,
-    private firestore: AngularFirestore // Adicione a injeção do AngularFirestore
+    private firestore: AngularFirestore
   ) { }
 
   async uploadAvatarAndSetUserProfile(imageFile: File) {
     const storageRef = firebase.storage().ref();
     const avatarRef = storageRef.child(`Avatar/${Date.now()}_${imageFile.name}`);
-
+  
     try {
+      console.log('Iniciando o upload do avatar...');
       const snapshot = await avatarRef.put(imageFile);
+      console.log('Upload do avatar concluído.');
+  
+      console.log('Obtendo a URL de download do avatar...');
       const downloadURL = await snapshot.ref.getDownloadURL();
+      console.log('URL de download do avatar obtida:', downloadURL);
+  
+      console.log('Atualizando o perfil do usuário com a nova URL do avatar...');
       await this.updateUserProfile(downloadURL);
-      console.log('Avatar atualizado com sucesso:', downloadURL);
+      console.log('Perfil do usuário atualizado com sucesso.');
+  
+      console.log('Obtendo o ID do usuário atual...');
+      const userId = await this.getCurrentUserId();
+      console.log('ID do usuário atual obtido:', userId);
+  
+      if (userId) {
+        console.log('Atualizando o documento do usuário com a nova URL do avatar...');
+        const userRef = this.firestore.doc(`users/${userId}`);
+        await userRef.update({ photoURL: downloadURL });
+        console.log('Documento do usuário atualizado com sucesso.');
+      }
     } catch (error) {
       console.error('Erro ao atualizar o avatar:', error);
       throw new Error('Falha ao fazer upload e atualizar o avatar.');
@@ -38,16 +56,24 @@ export class AuthService {
     return this.afAuth.signInWithEmailAndPassword(email, password);
   }
 
-registerWithEmail(email: string, password: string) {
-    return this.afAuth.createUserWithEmailAndPassword(email, password);
-      }
+  async registerWithEmail(email: string, password: string) {
+    const credential = await this.afAuth.createUserWithEmailAndPassword(email, password);
+    if (credential.user) {
+      const userRef = this.firestore.doc(`users/${credential.user.uid}`);
+      const userData = {
+        uid: credential.user.uid,
+        email: credential.user.email ?? '',
+      };
+      await userRef.set(userData, { merge: true });
+    }
+    return credential;
+  }
 
   loginWithGoogle() {
     return this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
 
   registerWithGoogle() {
-    // O método signInWithPopup é usado tanto para login quanto para registro com o Google
     return this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
 
@@ -66,35 +92,25 @@ registerWithEmail(email: string, password: string) {
         photoURL: url
       }).then(() => {
         console.log('Perfil atualizado com sucesso com a nova URL do avatar.');
-        // Aqui você pode adicionar lógica adicional após a atualização bem-sucedida.
       }).catch((error) => {
         console.error('Erro ao atualizar o perfil:', error);
       });
     });
   }
 
-  async getCurrentUserProfile(): Promise<{email: string, photoURL: string}> {
-    const user = await this.afAuth.currentUser;
-    if (!user) {
-      throw new Error('No user logged in');
+  async getCurrentUserProfile() {
+    const user = firebase.auth().currentUser;
+    if (user) {
+      const userDoc = await firebase.firestore().doc(`users/${user.uid}`).get();
+      if (userDoc.exists) {
+        const userData = userDoc.data();
+        console.log('Dados do usuário:', userData);
+        return userData;
+      } else {
+        throw new Error('Documento do usuário não encontrado');
+      }
+    } else {
+      throw new Error('Nenhum usuário logado');
     }
-            return {
-      email: user.email ?? '',
-      photoURL: user.photoURL ?? 'assets/default-avatar.png'
-    };
   }
-
-  async saveAvatarUrl(url: string) {
-    const userId = await this.getCurrentUserId();
-    if (!userId) {
-      throw new Error('User not logged in');
-    }
-
-    // Salvar a URL do avatar no documento do usuário no Firestore
-    return this.firestore.collection('users').doc(userId).update({
-      avatarUrl: url
-    });
-  }
-
-  // ... Outros métodos úteis como verificar status de autenticação
 }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -9,7 +9,7 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-  user = { email: '', avatarUrl: '' };
+  user = { email: '', photoURL: '' }; // Altere avatarUrl para photoURL
   isUserLoggedIn = false;
   senhaCriptografada = '••••••';
 
@@ -31,9 +31,16 @@ export class Tab4Page implements OnInit {
 
   loadUserProfile() {
     this.authService.getCurrentUserProfile().then(profile => {
-      this.isUserLoggedIn = !!profile.email;
-      this.user.email = profile.email;
-      this.user.avatarUrl = profile.photoURL || 'assets/default-avatar.png';
+      if (profile) {
+        console.log('Perfil do usuário:', profile);
+        if ('email' in profile) {
+          this.isUserLoggedIn = !!profile['email'];
+          this.user.email = profile['email'];
+        }
+        if ('photoURL' in profile) {
+          this.user.photoURL = profile['photoURL'] || 'assets/default-avatar.png';
+        }
+      }
     }).catch(error => {
       console.error('Erro ao carregar perfil:', error);
       this.showToast('Erro ao carregar o perfil do usuário. Por favor, tente novamente.');
@@ -47,11 +54,11 @@ export class Tab4Page implements OnInit {
   async sair() {
     await this.authService.logout();
     // Limpa as informações do usuário e define como deslogado
-    this.user = { email: '', avatarUrl: '' };
+    this.user = { email: '', photoURL: '' }; // Altere avatarUrl para photoURL
     this.isUserLoggedIn = false;
     await this.showToast('Usuário deslogado com sucesso');
     this.router.navigateByUrl('/tabs/tab1');
-}
+  }
 
   async showToast(message: string) {
     const toast = await this.toastController.create({
